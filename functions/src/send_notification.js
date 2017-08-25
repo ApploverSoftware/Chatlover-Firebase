7072fea6525bf811c6c15c26823fc3f8a228388f
sp0rk <julian.jurec@gmail.com>
2017-08-25 13:52:07 +0200

    Modified notification payloads for different message types

--- functions/src/send_notification.js
+++ functions/src/send_notification.js
@@ -2,25 +2,60 @@ module.exports = (db, admin) => {
     var module = {}
 
     module.run = (event) => {
-    	return db.ref(`/channels/${event.params.channelId}`).once('value').then(channelSnap => {
-        const channel = channelSnap.val();
-        const message = event.data.val();
-        const payload = {
-            notification: {
-                title: channel.name,
-                body: message.body
-            },
-            data: {
-                channelId: channel.id,
-                messageId: message.id
+    	return Promise.all([
+            db.ref(`/channels/${event.params.channelId}`).once('value'),
+            db.ref(`/chat_users/${event.data.val().sender}`).once('value')
+        ]).then(snaps => {
+            const channel = snaps[0].val();
+            const sender = snaps[1].val()
+            const message = event.data.val();
+            var notification = {}
+            switch (message.type) {
+                case 'txt':
+                    notification = {
+                        title: `${sender.name} pisze...`,
+                        body: message.body
+                    }
+                break;
+                case 'loc':
+                    notification = {
+                        title: `${sender.name} wysyła swoją lokalizację`,
+                        body: "Kliknij, aby ją zobaczyć"
+                    }
+                break;
+                case 'img':
+                    notification = {
+                        title: `${sender.name} wysyła zdjęcie`,
+                        body: "Kliknij, aby je zobaczyć"
+                    }
+                break;
+                case 'vid':
+                    notification = {
+                        title: `${sender.name} wysyła wideo`,
+                        body: "Kliknij, aby je obejrzeć"
+                    }
+                break;
+                case 'mic':
+                    notification = {
+                        title: `${sender.name} wysyła wiadomość głosową`,
+                        body: "Kliknij, aby ją odsłuchać"
+                    }
+                break;
             }
-        };
-        const tokens = Object.keys(channel.users)
-            .map(key => channel.users[key])
-            .filter(u => u.uid != message.sender)
-            .map(u => u.fcmToken)
-        admin.messaging().sendToDevice(tokens, payload).then(_ => console.log(`notification sent to ${tokens}`));
-    });
+            const payload = {
+                notification: notification,
+                data: {
+                    channel: channel,
+                    message: message,
+                    sender: sender
+                }
+            };
+            const tokens = Object.keys(channel.users)
+                .map(key => channel.users[key])
+                .filter(u => u.uid != message.sender)
+                .map(u => u.fcmToken)
+            admin.messaging().sendToDevice(tokens, payload).then(_ => console.log(`notification sent to ${tokens}`));
+        });
     }
 
     return module
